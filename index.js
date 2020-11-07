e8adfe4063e5fdf5cdf84e69cf3459d042f68dc3
kimberly3388 <kimberly.jaz@gmail.com>
2020-11-07 14:44:32 +0100

    Node no errors

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
-'use strict';
+var express = require('express');
+var app = express();
 
-var server = require('./src/backend/index');
-var logger = require('./src/backend/logger');
+app.use(express.static('public'))
 
-//if your project is a npm module:
-
-module.exports = server;
-
-//if your is not npm module
-
-server.myPromiseFunction("1", "2").then(function (solution) {
-    logger.info(solution);
-}, function (err) {
-    logger.error(err);
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+    //call this app from https://<workspace name>-<user name>.c9users.io
 });
-
-// quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint() {
-    logger.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
-    shutdown();
-});
-
-// quit properly on docker stop
-process.on('SIGTERM', function onSigterm() {
-    logger.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
-    shutdown();
-});
-
-// shut down server
-function shutdown() {
-    server.close(function onServerClosed(err) {
-        if (err) {
-            logger.error(err);
-            process.exitCode = 1;
-        }
-        process.exit();
-    });
-}
