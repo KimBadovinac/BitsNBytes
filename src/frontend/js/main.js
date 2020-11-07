bf53586271ba6654f1dc98c38bf7daa29f350996
kimberly3388 <kimberly.jaz@gmail.com>
2020-11-07 22:03:10 +0100

    Mobile navbar expanded first

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -3,7 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.padding = "30px 10px";
+        $('.navbar-collapse').collapse('hide');
     } else {
         document.getElementById("navbar").style.padding = "80px 10px";
+        $('.navbar-collapse').collapse('show');
     }
 }
