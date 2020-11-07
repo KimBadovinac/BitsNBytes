3795aeec67ece73487ba840c2194a727d493d23d
kimberly3388 <kimberly.jaz@gmail.com>
2020-11-07 15:09:18 +0100

    Fix

---
+++ README.md
@@ -0,0 +1,6 @@
+# instructions
+
+1. npm install
+2. node index.js
+
+http://localhost:8080/
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var app = express();
+const http = require("http"); // Use https if your app will not be behind a proxy.
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require('path');
 
-app.use(express.static('public'))
+const app = express();
 
-app.listen(8080, function () {
+const frontendPath = __dirname + '/src/frontend';
+app.use(express.static(frontendPath));
+//app.use(express.static('public'))
+
+app.listen(process.env.PORT || 8080, function () {
     console.log('Example app listening on port 8080!');
-    //call this app from https://<workspace name>-<user name>.c9users.io
+});
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,    Content-Type, Accept");
+    next();
+});
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join('frontendPath','index.html'));
 });
